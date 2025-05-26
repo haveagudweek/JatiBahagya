5b388beb2ee92ced0805199effb1749b9794cbf0
Yasir <justabigy@gmail.com>
2025-05-27 01:35:01 +0700

    Update entrypoint.sh
    mengembalikan command migrate

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ echo "Running entrypoint tasks..."
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan migrate:fresh --seed --force
+php artisan migrate --force
 echo "Entrypoint tasks complete. Starting server."
 exec "$@"
