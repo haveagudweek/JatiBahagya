c41cb7a64e52e73f6675d4f69adb6ba5115ac91a
Yasir <justabigy@gmail.com>
2025-05-26 23:42:51 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,22 +3,20 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
+use Illuminate\Http\Middleware\TrustProxies;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->trustProxies(
-            proxies: '*',
-            headers: Request::HEADER_X_FORWARDED_FOR |
-                     Request::HEADER_X_FORWARDED_HOST |
-                     Request::HEADER_X_FORWARDED_PORT |
-                     Request::HEADER_X_FORWARDED_PROTO |
-                     Request::HEADER_X_FORWARDED_AWS_ELB
-        );
+->withMiddleware(function (Middleware $middleware) {
+    $middleware->trustProxies(
+        headers: TrustProxies::HEADER_X_FORWARDED_FOR |
+         TrustProxies::HEADER_X_FORWARDED_HOST |
+         TrustProxies::HEADER_X_FORWARDED_PORT |
+         TrustProxies::HEADER_X_FORWARDED_PROTO|
+         TrustProxies::HEADER_X_FORWARDED_AWS_ELB);
         $middleware->web(append: [
             \App\Http\Middleware\CheckVerified::class,
         ]);
