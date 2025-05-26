e32ac97622b05759e16bb1d66eec23565dd12a14
Yasir <justabigy@gmail.com>
2025-05-26 23:57:22 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,25 +3,18 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\TrustProxies;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-->withMiddleware(function (Middleware $middleware) {
-    $middleware->trustProxies(
-        headers: TrustProxies::HEADER_X_FORWARDED_FOR |
-         TrustProxies::HEADER_X_FORWARDED_HOST |
-         TrustProxies::HEADER_X_FORWARDED_PORT |
-         TrustProxies::HEADER_X_FORWARDED_PROTO|
-         TrustProxies::HEADER_X_FORWARDED_AWS_ELB);
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->web(append: [
             \App\Http\Middleware\CheckVerified::class,
         ]);
-
-        // Atau sebagai alias middleware
         $middleware->alias([
             'verified' => \App\Http\Middleware\CheckVerified::class,
         ]);
